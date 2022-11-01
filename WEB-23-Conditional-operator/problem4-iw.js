587808fa8ecfc7800b67f7115784c88969d4e007
Nagaratnap <nagratnapatil411@gmail.com>
2022-11-01 23:54:35 +0530

    pushing day2 assignment problems from secondlocation

---
+++ WEB-23-Conditional-operator/problem4-iw.js
@@ -0,0 +1,10 @@
+let database_user="xyz12@gmail.com";
+let database_pass="123abc";
+
+let user="xyz12@gmail.com";
+let pass="123abc";
+if(database_user==user){
+  if(database_pass==pass){
+    console.log("Login Successfull");
+  }
+}
